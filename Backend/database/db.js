92676f8660dd4a3f57db0fe5c916b099d9f52186
Aditya Jha <adityajhajee7889@gmail.com>
2024-03-20 12:12:16 +0530

    initial commit

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,6 +1,7 @@
 // db.js
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://0.0.0.0:27017/CSP');
+mongoose.connect('mongodb+srv://amandeeprewani21:gwkX858P5alUkgte@cluster0.dahsegz.mongodb.net/CSP?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
+// Aditya28012002
